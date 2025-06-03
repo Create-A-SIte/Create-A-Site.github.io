d6f7c6887e1c0d379c32eb420f05cf4b634abc06
Create-A-Site-Official <Create.A.Site.Official@gmail.com>
2025-06-03 10:34:49 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'create-a-site-v2.11'; // Update version with each deployment
+const CACHE_NAME = 'create-a-site-v2.12'; // Update version with each deployment
 const urlsToCache = [
   '/',
   '/index.html',
